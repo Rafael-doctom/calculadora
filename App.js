6afff63465b9e9a4f46b2462923b11220033b2ce
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2021-12-23 04:28:11 -0300

    -- add layout project

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { TextInput } from 'react-native-web';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello, World!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.content}>
+        <Text style={styles.label}>Altura</Text>
+        <TextInput placeholder="Digite sua altura" keyboardType="numeric" style={styles.inputNumber}/>
+        <Text style={styles.label}>Peso:</Text>
+        <TextInput placeholder="Digite seu peso:" keyboardType="numeric" style={styles.inputNumber}/>
+      </View>
     </View>
   );
 }
@@ -15,6 +20,27 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
+    textAlign: 'left',
     justifyContent: 'center',
+  },
+  content: {
+    textAlign: 'left',
+    border: '1px solid blue',
+    padding: '2rem',
+    width: '25vw',
+    
+  },
+  label: {
+    textAlign: 'left',
+    color:'red',
+    paddingVertical: '5px'
+  },
+  inputNumber: {
+    textAlign: 'left',
+    border: '1px solid gray',
+    padding:'1rem',
+    height: '4rem',
+    with: '15vw',
+    
   },
 });
